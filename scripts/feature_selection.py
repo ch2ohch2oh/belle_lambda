3703f2b584ab68bace6c5213202df68a2c9b106c
Dazhi Wang <dazhi.wang@desy.de>
2019-11-13 23:55:12 +0900

    Now we use multiprocessing to remove features in paralell

--- scripts/feature_selection.py
+++ scripts/feature_selection.py
@@ -7,8 +7,11 @@ import pandas as pd
 from PyFastBDT import FastBDT
 from sklearn.metrics import roc_auc_score, roc_curve, average_precision_score, precision_recall_curve, recall_score
 import argparse
+import multiprocessing
 
+# Otherwise there would be an error if you are running in the background
 matplotlib.use('Agg')
+
 pd.set_option('display.max_columns', None)
 plt.rc('figure', figsize = (8, 6))
 plt.rc('font', size = 14)
@@ -25,6 +28,25 @@ def plot_auc_chart(baseline, df):
     plt.title('1 - AUC')
     return fig
 
+def worker(reduced, data, workerID = None):
+    """Worker process
+    
+    reduced:
+        Feature subset
+    data:
+        Data
+    """
+    # Make a copy of data
+    data = data.copy()
+    bdt = FastBDT.Classifier()
+    bdt.fit(data[reduced], data.isSignal)
+    data['mva'] = bdt.predict(data[reduced])
+    auc = roc_auc_score(data.isSignal, data.mva)
+    if workerID is not None:
+        print("Worker %d finished the job!" % workerID)
+    return {'auc': auc}
+
+
 def backward_selection(features, data, min_auc = 0.9975):
     # Make a copy of features so we do not accidentally modify them
     features = features.copy()
@@ -40,16 +62,22 @@ def backward_selection(features, data, min_auc = 0.9975):
     current_auc = init_auc
     best = pd.DataFrame()
     best = best.append({'n_features':len(features), 'best_auc':current_auc}, ignore_index = True)
+    
     while current_auc >= min_auc:
         print("Trying to remove one feature...")
         result = pd.DataFrame()
+        n_workers = 10
+        pool = multiprocessing.Pool(n_workers)
+        worker_results = []
         for i, _ in enumerate(features):
             reduced = [features[j] for j in range(len(features)) if j != i]
-            bdt = FastBDT.Classifier()
-            bdt.fit(data[reduced], data.isSignal)
-            data['mva'] = bdt.predict(data[reduced])
-            auc = roc_auc_score(data.isSignal, data.mva)
-            result = result.append({'feature': features[i], 'auc': auc}, ignore_index = True)
+            worker_results.append(pool.apply_async(worker, [reduced, data]))
+            print('Worker[%d]: Trying to remove %s' % (i, features[i]))
+        print("Waiting for the workers to finish...")
+        for i, res in enumerate(worker_results):
+            print("Return value from worker %d is %r" % (i, res.get()))
+            result = result.append({'feature': features[i], **res.get()}, ignore_index = True)
+        print(result)
         result = result.sort_values(by = 'auc', ascending = False)
         current_auc = result.iloc[0].auc
         best = best.append({'n_features':len(features) - 1, 'best_auc':current_auc}, ignore_index = True)
@@ -60,11 +88,12 @@ def backward_selection(features, data, min_auc = 0.9975):
         print('Features left (%d):' % (len(features)))
         for f in features:
             print(f)
-        print("AUC table")
+        print("AUC table begins".center(40, '='))
         print(result)
+        print("AUC table ends".center(40, '='))
         
         fig = plot_auc_chart(init_auc, result)
-        fig.savefig('../models/auc_%d.png' % (len(features) - 1), bbox_inches = "tight")
+        fig.savefig('../models/multiproc/auc_%d.png' % (len(features)), bbox_inches = "tight")
         
     return best
 
@@ -89,4 +118,11 @@ if __name__ == '__main__':
     best = backward_selection(features, train)
     print("Best AUC for each number of features")
     print(best)
+    
+    # Make a plot
+    fig = plt.figure()
+    plt.plot(best.n_features, 1 - best.best_auc)
+    plt.xlabel('Number of features')
+    plt.ylabel('1 - AUC')
+    fig.savefig('../models/multiproc/best.png', bbox_inches = "tight")
     
